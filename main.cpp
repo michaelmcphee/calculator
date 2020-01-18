8ba428e83816d115917dc2d5cc75900c556794d6
michaelmcphee <60047177+michaelmcphee@users.noreply.github.com>
2020-01-18 17:37:29 -0500

    updated main.cpp with new changes

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ double A;
 double B;
 double C;
 string quadratics;
+string calculator;
 string bigboy;
-
 
 void roots() {
     double Bsquared = (B * B);
@@ -18,9 +18,9 @@ void roots() {
     double T = (-B + discriminant) / (2 * A);
     double H = (-B - discriminant) / (2 * A);
     if (discriminant < 0) {
-        cout << "no roots";
+        cout << "No roots";
     } else {
-        cout << "answers are " << T << ", " << H;
+        cout << "Answers are " << T << ", " << H;
     }
 }
 
@@ -44,7 +44,7 @@ void quad() {
         cout << "finding vertex...";
         vertex();
     } else if (quadratics == "roots") {
-        cout << "finding roots...";
+        cout << "finding roots...\n";
         roots();
     } else if (quadratics == "all") {
         cout << "finding all...\n";
@@ -52,18 +52,106 @@ void quad() {
         cout << "\n";
         roots();
     } else {
-        cout << "try again idiot";
+        cout << "try again idiot\n";
+     //   quad();
     }
+}
+void add() {
+    int size;
+    cout << "Enter amount of numbers\n";
+    cin >> size;
+    int numbers[size];
+    cout << "Enter numbers with spaces between\n";
+    for(int i=0; i < size; i++){
+        cin >> numbers[i];
+    }
+    double sum = 0;
+    for(int counter = 0; counter < size; counter++){
+        sum = sum + numbers[counter];
+    };
+
+    cout << "Sum is " << sum;
 
 }
 
+void subtract() {
+    double x;
+    double y;
+    cout << "Enter two numbers with spaces between\n";
+    cin >> x >> y;
+    double z=x-y;
+    cout << "Difference is " << z;
 
+}
+
+void multiply() {
+    int size;
+    cout << "Enter amount of numbers\n";
+    cin >> size;
+    int numbers[size];
+    cout << "Enter numbers with spaces between\n";
+    for(int i=0; i < size; i++){
+        cin >> numbers[i];
+    }
+    double product = 1;
+    for(int counter = 0; counter < size; counter++){
+        product = product * numbers[counter];
+    };
+
+    cout << "Product is " << product;
+}
+void divide() {
+    double x;
+    double y;
+    cout << "Enter two numbers with spaces between\n";
+    cin >> x >> y;
+    double z=x/y;
+    cout << "Difference is " << z;
+
+}
+void calc() {
+
+    cout << "Enter function\n";
+    cin >> calculator;
+    if (calculator== "add") {
+        add();
+    } else if (calculator== "subtract") {
+        subtract();
+    } else if (calculator== "multiply") {
+        multiply();
+    } else if (calculator== "divide") {
+        divide();
+    } else {
+        cout << "try again idiot\n";
+        calc();
+    }
+}
+
+void line() {
+
+    double slopeone;
+    double slopetwo;
+    double yone;
+    double ytwo;
+    cout << "Enter slopes and y-intercepts with spaces in between\n";
+    cin >> slopeone >>yone >>slopetwo >>ytwo;
+    double x = (-yone+ytwo)/(slopeone-slopetwo);
+    double y = x*slopeone + yone;
+    cout << "Point of intersection is (" << x <<"," << y << ")";
+
+}
 
 int main() {
-    cout << "Enter quadratics, linear, or trigonometry";
+    cout << "Enter quadratics, linear, or calculator\n";
     cin >> bigboy;
     if (bigboy == "quadratics") {
         quad();
+    } else if (bigboy=="calculator") {
+        calc();
+    } else if (bigboy == "linear") {
+        line();
+    }else {
+        cout << "try again idiot \n";
+        main();
     }
-
 }
